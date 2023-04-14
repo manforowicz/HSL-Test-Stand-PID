38f045f27737c7d5816368c976f4eaa989eecbcc
Marcin Anforowicz <manforowicz@gmail.com>
2023-04-14 16:06:46 -0700

    Added friction

--- magnetorquer-demo/magnetorquer-demo.ino
+++ magnetorquer-demo/magnetorquer-demo.ino
@@ -1,38 +1,46 @@
-const int IN_1 = 3;
-const int IN_2 = 4;
+const int OUT_1 = 3;
+const int OUT_2 = 4;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(IN_1, OUTPUT);
-  pinMode(IN_2, OUTPUT);
-  
-
+  pinMode(OUT_1, OUTPUT);
+  pinMode(OUT_2, OUTPUT);
 }
 
-void positive() {
-  digitalWrite(IN_1, LOW);
-  digitalWrite(IN_2, HIGH);
+/**
+ * Sends a pwm to the magnetorquer.
+ *
+ * 0: Cuts off the coil.
+ * 255:  Full Out 1
+ * -255: Full Out 2
+ */
+void send(int power) {
+  analogWrite(OUT_1, constrain(power, 0, 255));
+  analogWrite(OUT_2, constrain(-power, 0, 255));
 }
 
-void negative() {
-  digitalWrite(IN_1, HIGH);
-  digitalWrite(IN_2, LOW);
-}
-
+/**
+ * Shorts the two ends of the coils,
+ * isolating them from outside power.
+ *
+ * May apply a dampening effect?
+ *
+ */
 void brake() {
-  digitalWrite(IN_1, HIGH);
-  digitalWrite(IN_2, HIGH);
+  digitalWrite(OUT_1, HIGH);
+  digitalWrite(OUT_2, HIGH);
 }
 
-void off() {
-  digitalWrite(IN_1, LOW);
-  digitalWrite(IN_2, LOW);
+void power_with_friction(int seconds, int power) {
+  for (int i = 0; i < seconds; i++) {
+    send(power);
+    delay(800);
+    brake();
+    delay(200);
+  }
 }
 
 void loop() {
-  positive();
-  delay(30 * 1000);
-  negative();
-  delay(30 * 1000);
-
+  power_with_friction(40, 255);
+  power_with_friction(40, -255);
 }
